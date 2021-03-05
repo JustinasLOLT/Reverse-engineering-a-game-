5ab3dd2fc4cc01f346f3beb17ff12638d1cbccc0
JustinasLOLT <58081173+JustinasLOLT@users.noreply.github.com>
2021-03-05 22:53:23 +0200

    Just A Up

--- my/script.js
+++ my/script.js
@@ -1,7 +1,7 @@
-const canvas = document.createElement('canvas');
+const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-canvas.width = 1024;
-canvas.height = 1024;
+canvas.width = 800;
+canvas.height = 500;
 
 const key = [];
 
@@ -18,5 +18,37 @@ const player = {
 
 const playerSprite = new Image();
 playerSprite.src = 'img/albertsimon.png';
+const background = new Image();
+background.src = 'img/update.png';
 
-//https://www.youtube.com/watch?v=EYf_JwzwTlQ 9:36
+function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
+  ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+  drawSprite(
+    playerSprite,
+    0,
+    0,
+    player.width,
+    player.height,
+    player.x,
+    player.y,
+    player.width,
+    player.height
+  );
+  requestAnimationFrame(animate);
+}
+
+animate();
+
+//https://www.youtube.com/watch?v=EYf_JwzwTlQ 17:00
+window.addEventListener('keydown', function (e) {
+  keys[e.keyCode] = true;
+  console.log(keys);
+});
+window.addEventListener('keyup', function (e) {
+  delete keys[e.keyCode];
+});
